ebbe726d80633733239521ef7150c3ec163c16d4
Kenya <kenyacobbs9001@gmail.com>
2026-03-18 18:56:38 -0400

    Added player rectangle and movement

--- game.js
+++ game.js
@@ -1,36 +1,20 @@
-// kaplay();
-// loadBean();
+// const canvas = document.getElementById("game"); //keeping this just in case
 
-kaplay({
-    width: 200,
-    height: 200, 
-    background: '#CFE3FC', //so you cannot just put a color you have to give a hex code
-    scale: 2, 
-    // canvas: document.getElementById("canvas") //creates a canvas that isn't listening to input
-});
+kaplay ({
+    width: 650,
+    height: 650,
+    // scale: 2,
+    background: '#CFE3FC',
+    // stretch: true,
+    // letterbox: true,
+})
 
-
-
-// const obj = add([
-//     rect(32,32), //draws object as a rectangle 
-//     pos(10, 20), // position the object in the x:10 and y:20 postion
-//     "shape", //classify the object as shape
-// ]);
-
-
-scene("red_bean", () => {
-    add([
-        rect(20, 20),
+  const player = add([
+        rect(30, 30),
         pos(40, 40),
-        color(245, 0, 0),
+        color(0,0,255),
+        move(screenLeft, 100),
     ]);
-
-    onKeyPress(() => {
-       debug.log("key pressed");
-    });
-});
-
-go("red_bean");
 
 //Setting up my scenes to have 6 rooms/screens
 scene("wake_up", () => {
@@ -56,26 +40,3 @@ scene("second_npc", () => {
 scene("end", () => {
 
 })
-
-
-//This was to make sure things worked. 
-// kaplay({
-//     width: 200,
-//     height: 200, 
-//     background: '#d46eb3',
-//     scale: 2,
-// });
-
-// scene("red_bean", () => {
-//     add([
-//         rect(32, 32),
-//         pos(50, 50),
-//         color(255, 0, 0),
-//     ]);
-
-//     onKeyPress(() => {
-//         debug.log("key pressed");
-//     });
-// });
-
-// go("red_bean");
